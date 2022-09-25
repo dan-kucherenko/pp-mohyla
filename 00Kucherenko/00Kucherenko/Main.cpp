0e4e1e844c4480e0ebe399545fd60c1b8876167e
Daniil Kucherenko <daniil.ukraine@gmail.com>
2022-09-25 19:54:16 +0300

    Improved tests and documentation of the function output

--- 00Kucherenko/00Kucherenko/Main.cpp
+++ 00Kucherenko/00Kucherenko/Main.cpp
@@ -13,22 +13,27 @@ using namespace std;
 
 int main(void) {
 	//Ex1
-	const int n = 5, ex1_x = 6;
-	cout << "Result of the 1st ex. (sum) is: " << sumFunc(&ex1_x, &n) << ", n=" << n << ", x=" << ex1_x << endl;
+	int n = 5, ex1_x = 6;
+	for (; ex1_x < 30; ex1_x += 5)
+		cout << "Result of the 1st ex. (sum) is: " << sumFunc(&ex1_x, &n) << ", n=" << n << ", x=" << ex1_x << endl;
+	cout << "==================================================================================" << endl;
 	//Ex2
 	double ex2_x = 0.7, e = 0.007;
-	cout << "Result of the 2nd ex.(sin func) for x=0.7 and e=0.007 is " << sinFunct(&ex2_x, &e) << ':' << sin(ex2_x) << endl;
+	cout << "Result of the 2nd ex.(sin func) for x=" << ex2_x << " and e=" << e << " is " << sinFunct(&ex2_x, &e) << ": default function check : " << sin(ex2_x) << endl;
+	cout << "==================================================================================" << endl;
 	//Ex3
 	const int dividend = 50, divisor = 4;
-	cout << "Result of the 3rd ex. (dividing with remainder) for m=-50, n=4 is " << divWithRem(dividend, divisor)
-		<< ", default function result is " << dividend % divisor  << endl;
+	cout << "Result of the 3rd ex. (dividing with remainder) for m=" << dividend << " n=" << divisor << " is " << divWithRem(dividend, divisor)
+		<< ", default function result is " << dividend % divisor << endl;
+	cout << "==================================================================================" << endl;
 	//Ex4
 	const int firstNum = 60, secondNum = 6;
-	cout << "Result of the 4th ex.(NSD) for 46 and 4 is " << nsdLoop(firstNum, secondNum) << 
+	cout << "Result of the 4th ex.(NSD) for " << firstNum << " and " << secondNum << " is " << nsdLoop(firstNum, secondNum) <<
 		", reccursive version of this function result is " << nsdReccursive(firstNum, secondNum) << endl;
+	cout << "==================================================================================" << endl;
 	//Ex5
 	const int ex5_var1 = 4, ex5_var2 = 3;
-	cout << "Result of the 5th ex. (Chebyshev Polynomial) for n=4 and x=3 is: " << chebyshevPolynomial(ex5_var1, ex5_var2) << endl;
+	cout << "Result of the 5th ex. (Chebyshev Polynomial) for n=" << ex5_var1 << " and x=" << ex5_var2 << " is " << chebyshevPolynomial(ex5_var1, ex5_var2) << endl;
 
 	return 0;
 
