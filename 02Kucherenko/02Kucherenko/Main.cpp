44bb6429d0d4e0a99429cef038db5068f9b600cf
Daniil Kucherenko <daniil.ukraine@gmail.com>
2022-10-24 19:46:39 +0300

    02Kucherenko lab fixing

--- 02Kucherenko/02Kucherenko/Main.cpp
+++ 02Kucherenko/02Kucherenko/Main.cpp
@@ -5,14 +5,13 @@
 using namespace std;
 
 int main(void) {
-	int exponent = -3;
+	const double x = 7.5;
 	unsigned int num_of_steps = 0;
-	cout.precision(40);
-	for (double x =-5; x <= 0; x++) {
-	cout << "Power for x=" << x << " with base " << exponent << " is " << power(x, exponent, num_of_steps) << ", number of steps is " << num_of_steps << endl;
-	cout << "Recursive power funct for x=" << x << " with base " << exponent << " is " << power_recursive(x, exponent, num_of_steps) << ", number of steps is " << num_of_steps << endl;
-	cout << "Quick power for x=" << x << " with base " << exponent << " is " << quick_power(x, exponent, num_of_steps) << ", number of steps is " << num_of_steps << endl;
-	cout << "Recursive quick power funct for x=" << x << " with base " << exponent << " is " << quick_power_recursive(x, exponent, num_of_steps) << ", number of steps is " << num_of_steps << endl;
-	cout << "==========================================================================" << endl << endl;
+	for (int exponent = 50; exponent <= 300; exponent +=25) {
+		cout << "Power: \t\t\t x=" << x << ", base=" << exponent << ": " << power(x, exponent, num_of_steps) << ", number of steps: " << num_of_steps << endl;
+		cout << "Recursive power: \t x=" << x << ", base=" << exponent << ": " << power_recursive(x, exponent, num_of_steps) << ", number of steps: " << num_of_steps << endl;
+		cout << "Quick power: \t\t x=" << x << ", base=" << exponent << ": " << quick_power(x, exponent, num_of_steps) << ", number of steps: " << num_of_steps << endl;
+		cout << "Recursive quick power: \t x=" << x << ", base=" << exponent << ": " << quick_power_recursive(x, exponent, num_of_steps) << ", number of steps: " << num_of_steps << endl;
+		cout << "==========================================================================" << endl << endl;
 	}
 }
