a4d19cf536bd3f7a6800bf89f1a9e9445a79851e
Daniil Kucherenko <daniil.ukraine@gmail.com>
2022-11-05 00:06:29 +0200

    Refactored code. Template sorting implemented

--- 06Kucherenko/06Kucherenko/Main.cpp
+++ 06Kucherenko/06Kucherenko/Main.cpp
@@ -5,12 +5,36 @@ using namespace std;
 
 int main(void) {
 	const size_t size = 7;
+
 	const int* arr_integer = new int[size] {3, 321, 3453, 123, 1, 53, 4356};
 	const int* sorted_arr = insertion_sort<int>(arr_integer, size);
-	print_arr(arr_integer, size);
 
-	/*const char* arr_character = new char[size] {'a', 'c', 'd', 'b', 'q', 'x', 'y'};
-	const char* sorted_char_arr = insertion_sort(arr_character, size);*/
+	cout << "Integer array: ";
+	print_arr(arr_integer, size);
+	cout << "Sorted int arr: ";
+	print_arr(sorted_arr, size);
+	delete[] arr_integer;
+	delete[] sorted_arr;
+	cout << "=========================================================" << endl;
+	const char* arr_char = new char[size] {'q', 'a', 'c', 'd', 'y', 'z', 'e'};
+	const char* sorted_char_arr = insertion_sort(arr_char, size);
+
+	cout << "Char array: ";
+	print_arr(arr_char, size);
+	cout << "Sorted char arr: ";
+	print_arr(sorted_char_arr, size);
+	delete[] arr_char;
+	delete[] sorted_char_arr;
+	cout << "=========================================================" << endl;
+	const double* arr_double = new double[size] {2.4, 1.4, 0.123, 0.01, 0.15, 34.57, 15.75};
+	const double* sorted_double_arr = insertion_sort<double>(arr_double, size);
+
+	cout << "Double array: ";
+	print_arr(arr_double, size);
+	cout << "Sorted double arr: ";
+	print_arr(sorted_double_arr, size);
+	delete[] arr_double;
+	delete[] sorted_double_arr;
 
 	return 0;
 }
--- 06Kucherenko/06Kucherenko/Sorter.h
+++ 06Kucherenko/06Kucherenko/Sorter.h
@@ -1,6 +1,47 @@
 #pragma once
-template <class T>
-T* insertion_sort(const T* arr, const size_t size);
+#include <iostream>
+#include <cassert>
 
 template <class T>
-void print_arr(const T* arr, const size_t size);
+bool is_arr_same(const T* input_arr, const T* sorted_arr) {
+	return true;
+}
+
+template <class T>
+bool sorted_in_ascending(const T* arr, const size_t arr_size) {
+	for (size_t i = 0; i < arr_size - 1; i++) {
+		if (arr[i + 1] < arr[i])
+			return false;
+	}
+	return true;
+}
+
+template <class T>
+void print_arr(const T* arr, const size_t arr_size) {
+	std::cout << "( ";
+	for (size_t i = 0; i < arr_size; i++)
+		std::cout << arr[i] << ' ';
+	std::cout << ')' << std::endl;
+}
+
+template <class T>
+T* insertion_sort(const T* arr, const size_t arr_size) {
+	T* sorted_arr = new T[arr_size];
+	for (size_t i = 0; i < arr_size; i++)
+		sorted_arr[i] = arr[i];
+
+	for (size_t i = 1; i < arr_size; ++i) {
+		T key = sorted_arr[i];
+		size_t j = i - 1;
+		while (j >= 0 && key < sorted_arr[j]) {
+			T temp = sorted_arr[j];
+			sorted_arr[j] = sorted_arr[j + 1];
+			sorted_arr[j + 1] = temp;
+			--j;
+		}
+	}
+	assert(sorted_in_ascending(sorted_arr, arr_size));
+	//assert(is_arr_same());
+	//delete[] sorted_arr;
+	return sorted_arr;
+}
