b225a8e57fc41f73fb9d020d6175145806033d48
dan_kucherenko <daniil.ukraine@gmail.com>
2022-11-05 14:19:45 +0200

    Upd.

--- 06Kucherenko/06Kucherenko/Main.cpp
+++ 06Kucherenko/06Kucherenko/Main.cpp
@@ -7,34 +7,16 @@ int main(void) {
 	const size_t size = 7;
 
 	const int* arr_integer = new int[size] {3, 321, 3453, 123, 1, 53, 4356};
-	const int* sorted_arr = insertion_sort<int>(arr_integer, size);
-
-	cout << "Integer array: ";
-	print_arr(arr_integer, size);
-	cout << "Sorted int arr: ";
-	print_arr(sorted_arr, size);
-	delete[] arr_integer;
-	delete[] sorted_arr;
+	sort_tester(arr_integer, size);
+	delete[]arr_integer;
 	cout << "=========================================================" << endl;
 	const char* arr_char = new char[size] {'q', 'a', 'c', 'd', 'y', 'z', 'e'};
-	const char* sorted_char_arr = insertion_sort(arr_char, size);
-
-	cout << "Char array: ";
-	print_arr(arr_char, size);
-	cout << "Sorted char arr: ";
-	print_arr(sorted_char_arr, size);
+	sort_tester(arr_char, size);
 	delete[] arr_char;
-	delete[] sorted_char_arr;
 	cout << "=========================================================" << endl;
 	const double* arr_double = new double[size] {2.4, 1.4, 0.123, 0.01, 0.15, 34.57, 15.75};
-	const double* sorted_double_arr = insertion_sort<double>(arr_double, size);
-
-	cout << "Double array: ";
-	print_arr(arr_double, size);
-	cout << "Sorted double arr: ";
-	print_arr(sorted_double_arr, size);
+	sort_tester(arr_double, size);
 	delete[] arr_double;
-	delete[] sorted_double_arr;
 
 	return 0;
 }
--- 06Kucherenko/06Kucherenko/Sorter.h
+++ 06Kucherenko/06Kucherenko/Sorter.h
@@ -3,17 +3,35 @@
 #include <cassert>
 
 template <class T>
-bool is_arr_same(const T* input_arr, const T* sorted_arr) {
-	return true;
+bool is_arr_same(const T* input_arr, const T* sorted_arr, size_t arr_size) {
+	//input_arr = insertion_sort(input_arr, arr_size);
+	//for (int i = 0; i < arr_size; ++i) {
+	//	if (input_arr[i] != sorted_arr[i])
+	//		return false;
+	//}
+	//return true;
+	for (size_t i = 0; i < arr_size; ++i)
+		if (input_arr[i] == sorted_arr[0]) {
+			bool is_arr_same = true;
+			for (unsigned int j = 1; j < arr_size; ++j)
+				if (input_arr[i + j] != sorted_arr[j]) {
+					is_arr_same = false;
+					break;
+				}
+			return is_arr_same;
+		}
 }
 
 template <class T>
 bool sorted_in_ascending(const T* arr, const size_t arr_size) {
+	bool sorted_in_asc = true;
 	for (size_t i = 0; i < arr_size - 1; i++) {
-		if (arr[i + 1] < arr[i])
-			return false;
+		if (arr[i + 1] < arr[i]) {
+			sorted_in_asc = false;
+			break;
+		}
 	}
-	return true;
+	return sorted_in_asc;
 }
 
 template <class T>
@@ -41,7 +59,16 @@ T* insertion_sort(const T* arr, const size_t arr_size) {
 		}
 	}
 	assert(sorted_in_ascending(sorted_arr, arr_size));
-	//assert(is_arr_same());
-	//delete[] sorted_arr;
+	assert(is_arr_same(arr, sorted_arr, arr_size));
 	return sorted_arr;
+}
+
+template <class T>
+void sort_tester(const T* arr, size_t arr_size) {
+	T* sorted_arr = insertion_sort(arr, arr_size);
+	std::cout << "Array of " << typeid(T).name() << ':';
+	print_arr<T>(arr, arr_size);
+	std::cout << "Sorted arr of " << typeid(T).name() << ':';
+	print_arr(sorted_arr, arr_size);
+	delete[] sorted_arr;
 }
