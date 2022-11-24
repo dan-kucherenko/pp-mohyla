b2091e244e1b4fe71f66179160458c7ef3457c07
dan_kucherenko <daniil.ukraine@gmail.com>
2022-11-24 18:01:06 +0200

    Initial commit

---
+++ 09Kucherenko/09Kucherenko/Dichotomy.cpp
@@ -0,0 +1,5 @@
+#include "Dichotomy.h"
+
+double dichotomy(double(*f)(double), double a, double b, double eps) {
+    return 0.0;
+}
---
+++ 09Kucherenko/09Kucherenko/Dichotomy.h
@@ -0,0 +1,3 @@
+#pragma once
+double dichotomy(double (*f)(double), double a, double b, double eps);
+
